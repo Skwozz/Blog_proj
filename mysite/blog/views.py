cf39e6cc3828c9a9ddba48b62d3c252266c58529
Skwozz <rakalov.s@ya.ru>
2024-08-27 12:22:44 +0300

    Проект блога

---
+++ mysite/blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import PostForm
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', { 'posts': posts })
+
+
+def redirect(param):
+    pass
+
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', { 'form': form })
