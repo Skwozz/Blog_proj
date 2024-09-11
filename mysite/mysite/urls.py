0c77fb053983243e7f02a28568a7771908f6cae7
Skwozz <rakalov.s@ya.ru>
2024-09-11 19:10:44 +0300

    Доделана регистрация и аутентификация

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+# from mysite.users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('register/', include('users.urls')),
+    path('profile/', include('users.urls'), name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
